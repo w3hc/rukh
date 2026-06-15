30f50f243e7ccef1ac6f6e0f19980b32a21fef23
julienbrg <julien@strat.cc>
2026-06-15 15:33:20 +0200

    fix TS 6 config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        project: './tsconfig.json',
+        project: './tsconfig.eslint.json',
         sourceType: 'module',
       },
       globals: {
